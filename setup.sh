70eac17b6e0fb8d5c89493d9874471bc4cd4b46a
Kelsey Florek <kelseyflorek22@gmail.com>
2024-11-27 13:05:22 -0600

    added numbered backups for .zshrc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #Install ohmyzsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-cp --backup zshrc ~/.zshrc
+cp --backup=numbered zshrc ~/.zshrc
 
 #Install ohmyzsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
